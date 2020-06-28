d5823cc8da3754d26a5fae79d2d96c12c9207775
yinchun.liu <1101032620@qq.com>
2020-06-28 15:20:23 +0800

    fix: base输出组件安装错误修复

--- src/base.ts
+++ src/base.ts
@@ -5,8 +5,33 @@ import vClipboard from './directives/clipboard';
 import Password from './components/Password.vue';
 import './style/index.less';
 
+const components: any = {
+    Group,
+    Loading,
+    Password
+};
 
+const install: any = (Vue: any) => {
+    if (install.installed) return;
+
+    Object.keys(components).forEach((key: string) => {
+        Vue.component(key, components[key]);
+    });
+};
+
+// auto install
+if (typeof window !== 'undefined' && (window as any).Vue) {
+    install((window as any).Vue);
+}
+
+const API = {
+    install,
+    ...components
+};
+
+export default API;
 export {
+    install,
     Group,
     Loading,
     vLoading,
